93b24c11d1622c24f60c038fc550f2919ef533d1
Markus Felten <Markus.Felten@gmx.de>
2019-08-28 21:10:41 +0200

    fix: switch to light-server

--- rollup.config.js
+++ rollup.config.js
@@ -3,15 +3,7 @@ import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
 import json from "rollup-plugin-json";
 import { terser } from "rollup-plugin-terser";
-import autoPreprocess from "svelte-preprocess";
-import postcssImport from "postcss-import";
 import copy from 'rollup-plugin-copy';
-
-import history from "connect-history-api-fallback";
-import proxy from "http-proxy-middleware";
-import express from "express";
-import { create as browserSyncFactory } from "browser-sync";
-import { config } from './package.json';
 
 const production = !process.env.ROLLUP_WATCH;
 const dist = "public";
@@ -31,15 +23,6 @@ export default {
     }),
     svelte({
       dev: !production,
-
-      preprocess: autoPreprocess({
-        transformers: {
-          postcss: {
-            plugins: [postcssImport]
-          }
-        }
-      }),
-      css: css => css.write(`${dist}/bundle.css`)
     }),
 
     resolve({ browser: true }),
@@ -54,29 +37,3 @@ export default {
     clearScreen: false
   }
 };
-
-if (!production) {
-  function browsersync() {
-    const browserSync = browserSyncFactory();
-    const app = express();
-
-    app.use(
-      config.api,
-      proxy({
-        target: config.proxyTarget,
-        changeOrigin: true,
-        logLevel: "debug"
-      })
-    );
-
-    browserSync.init({
-      server: dist,
-      watch: true,
-      middleware: [app, history()]
-    });
-  }
-
-  setTimeout(() => {
-    browsersync();
-  }, 500);
-}
