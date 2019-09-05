e63ade2bdc177bfe9eaba1ab9262016091d1f1fb
Markus Felten <Markus.Felten@gmx.de>
2019-09-05 12:25:19 +0200

    fix: start light-server from rollup

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,30 @@ import commonjs from "rollup-plugin-commonjs";
 import json from "rollup-plugin-json";
 import { terser } from "rollup-plugin-terser";
 import copy from 'rollup-plugin-copy';
+import { spawn } from "child_process";
+import { config } from "./package.json";
 
 const production = !process.env.ROLLUP_WATCH;
 const dist = "public";
+
+if (!production) {
+  const ls = spawn("./node_modules/.bin/light-server", [
+    "-s",
+    dist,
+    "--servePrefix",
+    config.urlPrefix,
+    "--historyindex",
+    config.urlPrefix + "/index.html",
+    "-p",
+    "5000",
+    "--proxypath",
+    config.api,
+    "-x",
+    config.proxyTarget,
+    "-w", `${dist}/*.mjs,${dist}/*.css`
+  ]);
+  ls.stdout.pipe(process.stdout);
+}
 
 export default {
   input: "src/main.mjs",
